26cc77707beac38c0c08003da5bd7fcdb95d5d14
wayne <you@example.com>
2019-11-13 16:46:53 -0600

    added example html template and some routing

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+def test_page(request):
+    return render(request, 'blog/test_page.html', {})
