f7ec52b4c5f27a3741d6e33e7d19f0fdd4fb3136
wayne <you@example.com>
2019-11-13 20:02:05 -0600

    added post detail template

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('test', views.test_page, name='test_page'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -9,3 +9,7 @@ def post_list(request):
 
 def test_page(request):
     return render(request, 'blog/test_page.html', {})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
