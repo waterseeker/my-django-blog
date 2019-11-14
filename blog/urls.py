cc84c8850def7d5145fe4f8498936c67cc19f03b
wayne <you@example.com>
2019-11-13 20:25:43 -0600

    adding a form for creating/editing posts

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('test', views.test_page, name='test_page'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
 ]
